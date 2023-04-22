747da5b6396a8fb9712f7cdc6c247ecbfda9ea8d
Thomas Morton <sc21tam@leeds.ac.uk>
2023-04-22 22:50:07 +0100

    Started on phonemes to trunic conversion

--- src/test.py
+++ src/test.py
@@ -1,5 +1,6 @@
 from trunic import Trunic
 
 
-d = Trunic("Original script by Andrew Shouldice")
-d.toImage("hello", (1000, 2000), (255, 255, 255), 200, (0, 0, 0))
+d = Trunic("the")
+print(d.to_ipa("us"))
+# d.to_image("out", (1000, 2000), (255, 255, 255), 200, (0, 0, 0))
--- src/trunic.py
+++ src/trunic.py
@@ -5,6 +5,8 @@ from PIL import ImageFont, Image, ImageDraw
 
 
 class Trunic:
+    vowels = {"ʌ":"a", "aɪ":"ai", "aʊ":"au", "dʒ":"dj", "oʊ":"ou"}
+    conson = {"ɹ":"r"}
     font = os.path.join(os.path.dirname(__file__), "trunic.otf")
 
     def __init__(self, text: str) -> None:
@@ -18,15 +20,56 @@ class Trunic:
         print(self.us)
 
 
-    def toImage(self, 
-                output: str,
-                img_size: tuple[int, int],
-                color: str | tuple[int, int, int],
-                font_size: int,
-                font_color: str | tuple[int, int, int]) -> None:
+    def _check_locale(self, locale: str):
+        if locale != "gb" and locale != "us":
+            msg = "Unknown locale. Use either 'gb' or 'us'." 
+            raise ValueError(msg)
+        
+        if locale == "gb":
+            return self.gb
+        elif locale == "us":
+            return self.us
 
-        trunic = ImageFont.truetype(Trunic.font, font_size)
-        img = Image.new("RGBA", size=img_size, color=color)
-        draw = ImageDraw.Draw(img)
-        draw.text(xy=(10, 0), text="hello world", fill=font_color, font=trunic)
-        img.save(output + ".png")
+
+    def to_ipa(self, locale: str):
+        loc = self._check_locale(locale)
+
+        string = "/"
+        for i in loc:
+            for j in i:
+                string += j
+            string += " "
+
+        return string.strip() + "/"
+
+
+    """
+    Returns the string in the format specified by
+    https://github.com/dirdam/fonts/tree/main/tunic#how-to-use-the-font
+    """
+    def to_string(self, locale: str):
+        loc = self._check_locale(locale)
+
+        string = ""
+        for i in loc:
+            if i in dict.keys(Trunic.vowels):
+                pass
+            elif i in dict.keys(Trunic.conson):
+                pass
+            string += " "
+
+        return string.strip()
+
+
+    # def to_image(self, 
+    #             output: str,
+    #             img_size: tuple[int, int],
+    #             color: str | tuple[int, int, int],
+    #             font_size: int,
+    #             font_color: str | tuple[int, int, int]) -> None:
+
+    #     trunic = ImageFont.truetype(Trunic.font, font_size)
+    #     img = Image.new("RGBA", size=img_size, color=color)
+    #     draw = ImageDraw.Draw(img)
+    #     draw.text(xy=(10, 0), text=string, fill=font_color, font=trunic)
+    #     img.save(output + ".png")
