b0cb22557dbdc57177ec031ad2fceae1d6848c72
Thomas Morton <sc21tam@leeds.ac.uk>
2023-04-23 03:29:48 +0100

    Vowels and consonants of Trunic

--- src/test.py
+++ src/test.py
@@ -1,6 +1,4 @@
 from trunic import Trunic
 
 
-d = Trunic("the")
-print(d.to_ipa("us"))
-# d.to_image("out", (1000, 2000), (255, 255, 255), 200, (0, 0, 0))
+d = Trunic("paw")
--- src/trunic.py
+++ src/trunic.py
@@ -1,75 +1,79 @@
 import os
-from phonemizer import phonemize
-from phonemizer.separator import Separator
 from PIL import ImageFont, Image, ImageDraw
 
 
 class Trunic:
-    vowels = {"ʌ":"a", "aɪ":"ai", "aʊ":"au", "dʒ":"dj", "oʊ":"ou"}
-    conson = {"ɹ":"r"}
+    vowels = {
+        "æ":"a",      # back, sad
+        "ɑː":"ar",    # arm, large
+        "ɒ":"ah",     # swan, box
+        "eɪ":"ay",    # bay, game 
+        "ɛ":"e",      # end, pet
+        "iː":"ee",    # bee, team
+        "ɪəʳ":"eer",  # near, here 
+        "ə":"eh",     # the, about 
+        "eəʳ":"ere",  # air, vary
+        "ɪ":"i",      # bit, rich 
+        "aɪ":"ie",    # guy, life
+        "ɜːʳ":"ir",   # bird, work 
+        "oʊ":"oh",    # toe, over
+        "ɔɪ":"oi",    # toy, avoid    
+        "uː":"oo",    # too, june
+        "ʊ":"ou",     # wolf, good
+        "aʊ":"ow",    # how, your
+        "ʊəʳ":"ore"   # your, cure
+    }
+
+    conson = {
+        "b":"b",      # boss, baby 
+        "tʃ":"ch",    # chat, catch
+        "d":"d",      # dog, dad
+        "f":"f",      # fox, fail 
+        "g":"g",      # gun, bag
+        "h":"h",      # hop, house
+        "dʒ":"j",     # jam, judge 
+        "k":"k",      # cat, skip 
+        "l":"l",      # live, leaf
+        "m":"m",      # man, mime 
+        "n":"n",      # net, nun
+        "ŋ":"ng",     # rink, sing 
+        "p":"p",      # poppy, pip
+        "ɹ":"r",      # run, borrow    
+        "s":"s",      # sit, sass
+        "ʃ":"sh",     # shut, shoe
+        "t":"t",      # tunic, stop
+        "θ":"th",     # think, bath
+        "ð":"th",     # this, the
+        "v":"v",      # vine, verge
+        "w":"w",      # wow, worry
+        "j":"y",      # you, yes
+        "z":"z",      # zoo
+        "ʒ":"zh"      # vision
+    }
+
     font = os.path.join(os.path.dirname(__file__), "trunic.otf")
 
     def __init__(self, text: str) -> None:
-        sep = Separator(phone=" ", word="-")
-        self.gb = phonemize(text, language="en-gb", strip=True, separator=sep, preserve_punctuation=True)
-        self.us = phonemize(text, language="en-us", strip=True, separator=sep, preserve_punctuation=True)
-        self.gb = [i.split() for i in self.gb.split("-")]
-        self.us = [i.split() for i in self.us.split("-")]
-
-        print(self.gb)
-        print(self.us)
-
-
-    def _check_locale(self, locale: str):
-        if locale != "gb" and locale != "us":
-            msg = "Unknown locale. Use either 'gb' or 'us'." 
-            raise ValueError(msg)
-        
-        if locale == "gb":
-            return self.gb
-        elif locale == "us":
-            return self.us
-
-
-    def to_ipa(self, locale: str):
-        loc = self._check_locale(locale)
-
-        string = "/"
-        for i in loc:
-            for j in i:
-                string += j
-            string += " "
-
-        return string.strip() + "/"
+        pass
 
 
     """
     Returns the string in the format specified by
     https://github.com/dirdam/fonts/tree/main/tunic#how-to-use-the-font
     """
-    def to_string(self, locale: str):
-        loc = self._check_locale(locale)
-
-        string = ""
-        for i in loc:
-            if i in dict.keys(Trunic.vowels):
-                pass
-            elif i in dict.keys(Trunic.conson):
-                pass
-            string += " "
-
-        return string.strip()
+    def to_string(self):
+        pass
 
 
-    # def to_image(self, 
-    #             output: str,
-    #             img_size: tuple[int, int],
-    #             color: str | tuple[int, int, int],
-    #             font_size: int,
-    #             font_color: str | tuple[int, int, int]) -> None:
+    def to_image(self, 
+                output: str,
+                img_size,
+                color: str,
+                font_size: int,
+                font_color: str) -> None:
 
-    #     trunic = ImageFont.truetype(Trunic.font, font_size)
-    #     img = Image.new("RGBA", size=img_size, color=color)
-    #     draw = ImageDraw.Draw(img)
-    #     draw.text(xy=(10, 0), text=string, fill=font_color, font=trunic)
-    #     img.save(output + ".png")
+        trunic = ImageFont.truetype(Trunic.font, font_size)
+        img = Image.new("RGBA", size=img_size, color=color)
+        draw = ImageDraw.Draw(img)
+        draw.text(xy=(10, 0), text=self.to_string(), fill=font_color, font=trunic)
+        img.save(output + ".png")
